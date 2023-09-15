1d7bff0f36cb829952f29002436dffc3a840b23c
harsh589459 <hs589459@gmail.com>
2023-09-15 16:57:16 +0530

    crud done

--- models/product.js
+++ models/product.js
@@ -1,3 +1,31 @@
+const mongoose =require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    price:{
+        type:String,
+        retuired:true,
+    },
+
+    imageUrl:{
+        type:String,
+        required:true
+    }
+});
+
+module.exports = mongoose.model('Product',productSchema);
+
+
+
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
 
